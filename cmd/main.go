2dbbb604cc21e8ff17e59dddeef514f0dbb8ff68
Teresa Chu <teresa.chu@hashicorp.com>
2023-02-08 16:14:32 -0500

    Add main.go example usage file.

---
+++ cmd/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	apiclient "github.com/hashicorp/waypoint-client/client"
+	config "github.com/hashicorp/waypoint-client/config"
+	"github.com/hashicorp/waypoint-client/gen/client/waypoint"
+)
+
+//Example Usage of Waypoint Client Library with HCP Waypoint.
+
+func main() {
+
+	hcp, err := config.New("some_hcp_org_id",
+		"some_hcp_project_id",
+		"some_service_principal_client_id",
+		"some_client_secret")
+	if err != nil {
+		panic(err)
+	}
+
+	clientConfig := apiclient.Config{
+		HCPWaypointConfig: *hcp,
+		BasePath:          "/",
+	}
+
+	client, err := apiclient.New(clientConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.WaypointListProjects(waypoint.NewWaypointListProjectsParams())
+
+	fmt.Printf("%v#", resp.GetPayload())
+
+}
